4c8f2ae802a1608c0a1a130d29d65cdb7642ee9a
Rohit S <rohit254253@gmail.com>
2026-04-16 21:56:27 +0530

    Added procfile and app render

--- app.py
+++ app.py
@@ -1,42 +1,41 @@
 from dash import Dash, html, dcc
 from dash.dependencies import Input, Output
+import os
+
 from utils.data_loader import load_data
 from components.map import create_map
 from components.filters import create_filters
 
 # Initialize app
 app = Dash(__name__)
+server = app.server  # Required for deployment
 
 # Load data
 df = load_data()
 
 # Layout
 app.layout = html.Div([
-    
     html.H1("Terrorism Analysis Dashboard"),
-    
-    # Filters
     create_filters(df),
-    
-    # Map
     dcc.Graph(id='map')
 ])
 
-# Callback (Dropdown → Map)
+# Callback
 @app.callback(
     Output('map', 'figure'),
     Input('country', 'value')
 )
 def update_map(selected_country):
-    
     if selected_country:
         filtered_df = df[df['country_txt'] == selected_country]
     else:
         filtered_df = df
-
     return create_map(filtered_df)
-
 
 # Run app
 if __name__ == "__main__":
-    app.run(debug=False,host='0.0.0.0',port=10000)
+    app.run(
+        debug=False,
+        host='0.0.0.0',
+        port=int(os.environ.get("PORT", 10000))
+    )
